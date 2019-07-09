065064f25a183b147fd8c1fd7ba5a7b63bedab14
Camilo Andres Cortes <kmilo2106@hotmail.com>
2019-07-08 22:10:24 -0500

    Adding scaffolded and initial js/css files

---
+++ NSWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NSWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
